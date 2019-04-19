bc291be7a51929049f6c2f7c4f07d0f5a8726ab2
TSN Student <student@ulem.org>
2019-04-19 11:54:59 -0400

    US History

---
+++ Maine.js
@@ -0,0 +1,33 @@
+console.log("Good Morning Massachusetts!")
+alert("Coming to you live from Boston Mass...");
+// Question 1
+// Write the function printInt(n)
+// taking one parameter n and print all natural numbers
+// from 1 to n in console.
+function printInt(n) {
+    // Using a loop print from 1 to n 
+    // start: 1
+var i = 1 
+    // stop: n
+    while(i <= n){
+        console.log(i)
+    // step: +1
+        i++;
+    }
+}
+printInt(10);
+// Question 2
+// Write a function for the number of passangers 
+// on the US ship known as the "Maine" prior to WWI
+// Cross reference the attack on Hawaii 
+// That then started WWII 
+// function ...(){
+// store the answer
+// var solution=...;
+// loop over numbers from 1 to num
+// for(...;...;...){
+// check if current index is even, odd, or fractional
+// if(...) {}
+//}
+// return solution;
+//}
